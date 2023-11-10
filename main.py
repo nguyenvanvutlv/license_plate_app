7351f9e87807a59b36dfaaee4e44473a9b2fe46b
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-11-10 17:59:39 +0700

    update demo version 1.0.0

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
 import os
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
 from utils.opencv import (
     camera,
     draw,
@@ -7,40 +10,71 @@ from utils.opencv import (
 )
 import time
 from utils.lisence.detect import YoloNAS
+from utils.wpod.detect import Wpodnet
+import torch
+torch.cuda.set_device(0)
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class App:
     def __init__(self, video_source=0, window_name="lisence", flip=False) -> None:
-        self.video = camera.Camera(video_source)
+        # self.video = camera.Camera(video_source)
+        self.video = cv2.VideoCapture(video_source)
         self.fps = fps.FPS()
         self.window_name = window_name
         self.flip = flip
         self.max_fps = 144
 
-        self.yolonas = YoloNAS("model/ckpt_best.pth")
+        self.yolonas = YoloNAS("model/lisence/ckpt_best.pth")
         self.yolonas.load()
+        self.wpodnet = Wpodnet()
+        self.width = 1024
+        self.height = 720
+
+        self.cropped = None
+        self.output_wpod = None
+        self.thresh = None
 
     def process(self, image):
-        pass
+        img = image.copy()
+        img = imutils.resize(img, self.width, self.height)
+        _status, frame, _croped = self.yolonas.predict(img)
+        if _status is None:
+            return
+        self.cropped = _croped
+        self.output_wpod = self.wpodnet.run(
+            self.cropped).astype(np.float32) * 255
+
+        gray = cv2.cvtColor(self.output_wpod, cv2.COLOR_RGB2GRAY)
+        # blurred = cv2.GaussianBlur(gray, (7, 7), 0).astype((np.uint8))
+        self.thresh = cv2.adaptiveThreshold(
+            gray.astype((np.uint8)), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+        
+        
+        return frame
 
     def run(self):
-        self.video = self.video.start()
+        # self.video = self.video.start()
         try:
             while True:
                 self.fps.start()
-                frame = self.video.read()
+                # frame = self.video.read()
+                res, frame = self.video.read()
                 # if self.flip:
                 # frame = cv2.flip(frame, 1)
                 if frame is not None:
                     # CODE HERE
-
-                    _status, frame, _croped = self.yolonas.predict(frame)
+                    self.process(frame)
                     # CODE HERE
                     self.fps.stop()
                     self.fps.update()
                     frame = draw.putText(
                         frame, f'FPS: {self.fps.fps()}', (20, 20))
                     cv2.imshow(self.window_name, frame)
+                    if self.thresh is not None:
+                        cv2.imshow("thresh", self.thresh)
+
                     time.sleep(1 / self.max_fps)
                     if cv2.waitKey(1) & 0xFF == ord('x'):
                         break
@@ -51,11 +85,11 @@ class App:
         except RuntimeError as error:
             print(error)
         finally:
-            self.video.stop()
+            # self.video.stop()
             self.video.release()
             cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    app = App(flip=True)
+    app = App(video_source="data/abc.mp4", flip=True)
     app.run()
